6d65d4059c66f306906ccc0383be6a217a3404b8
Robert Taube <robertptaube@gmail.com>
2024-03-16 19:46:31 +0100

    Update 1. Two Sum.cpp

--- 1. Two Sum.cpp
+++ 1. Two Sum.cpp
@@ -1,16 +1,19 @@
-//Runtime 11 ms Beats 85.87%
-//Memory 11.7 MB Beats 11.22%
+// 16.03.24
+// Runtime 3ms Beats 98.74% of users with C++ 
+// Memory 15.58MB Beats 7.64% of users with C++
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-std::map<int,int> bucket;
-int n=nums.size();
-for(int i=0;i<n;i++){
-    if(bucket[target-nums[i]]){
-        return {bucket[target-nums[i]]-1, i};
-    }
-        bucket[nums[i]]=i+1;
-}
-        return {0,0}; 
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
+        std::unordered_map<int, std::pair<bool, int>> numbers; // 1st is number, 2nd is index
+        auto len = nums.size();
+        for(auto i = 0; i < len; ++i){
+            if(!numbers[target - nums[i]].first){
+                numbers[nums[i]].first = true;
+                numbers[nums[i]].second = i;
+            }else{
+                return {numbers[target - nums[i]].second, i};
+            }
+        }
+    return {0, 0};
     }
 };

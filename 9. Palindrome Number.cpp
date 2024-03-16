75c53df7e416387340d4c7d081698b93ce73fd3e
Robert Taube <robertptaube@gmail.com>
2024-03-16 20:17:58 +0100

    Update 9. Palindrome Number.cpp

--- 9. Palindrome Number.cpp
+++ 9. Palindrome Number.cpp
@@ -1,28 +1,22 @@
-//Runtime 4 ms Beats 97.94%
-//Memory 5.9 MB Beats 62.75%
+// 16.03.24
+// Runtime 0 ms Beats 100% of users with C++
+// Memory 8.21 MB Beats 45.42% of users with C++
 static const auto _ = []() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     return nullptr;
 }();
+
 class Solution {
 public:
     bool isPalindrome(int x) {
-       if(x<0) return false;
-       if(x<9) return true;
-       auto liczba = x;
-       int n {0};
-       while (liczba>0){
-        liczba/=10;
-        n++;
-       }
-       int tab[n];
-       for (int i=0; x>0; i++){
-        tab[i]=x%10;
-        x/=10;
-       }
-       for(int i=0; i<n; i++)
-       if(tab[i]!=tab[n-i-1]) return (i>=n-i-1)?true:false; 
-       return true;      
+        if (x < 0) return false;
+        unsigned int reversed{};
+        unsigned int copy = x;
+        while (x) {
+            reversed = reversed * 10 + x % 10;
+            x /= 10;
+        }
+        return reversed == copy;
     }
 };

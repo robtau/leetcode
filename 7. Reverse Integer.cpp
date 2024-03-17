fb205ecf07d599816644871b677ff6d6b5cc9254
Robert Taube <robertptaube@gmail.com>
2024-03-17 01:02:51 +0100

    Update 7. Reverse Integer.cpp

--- 7. Reverse Integer.cpp
+++ 7. Reverse Integer.cpp
@@ -1,13 +1,15 @@
+// 17.03.24
+// Runtime 0 ms Beats 100.00% of users with C++
+// Memory 7.52 MB Beats 14.88% of users with C++	
 class Solution {
 public:
-    int reverse(int x) {	
-        long long result=0;
-        while(x)
-        {
-            result = result * 10 + x % 10;
-            x = x /10;
-		}
-		if(result>INT_MAX || result<INT_MIN) return 0;		
-        return result;
-	}
+    int reverse(int x) {
+        long reversed{};
+        while (x) {
+            reversed = reversed * 10 + x % 10;
+            x /= 10;
+        }
+        if (abs(reversed) > std::numeric_limits<int>::max()) return 0;
+        return reversed;
+    }
 };
